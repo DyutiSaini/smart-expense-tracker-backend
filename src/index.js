7fe606ad56159979fae3c081026f209492d53f34
Dyuti Saini <sainidyuti@gmail.com>
2026-03-31 23:31:15 +0530

    fixed wrong env variable bug

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ dotenv.config();
 import { app } from "./app.js";
 // import mongoose from "mongoose";
 import connectdb from "./db/connectdb.js";
-const port = process.env.MONGO_URI || 8000;
+const port = process.env.PORT || 8000;
 connectdb().then(() => {
   app.listen(port, () => {
     console.log(`server is connecting at port ${port}`);
